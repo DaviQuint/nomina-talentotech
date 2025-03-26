fbe8923933f8342c6606826bc698dace5c45c8f1
DaviQuint <d.quintana0805@gmail.com>
2025-03-26 18:57:21 -0500

    Update libreria_nomina.py
    Algunas opiniones sobre cambios a realizar, pero si no se aplican igualmente estaría bien, ya son solo opiniones

--- libreria_nomina.py
+++ libreria_nomina.py
@@ -11,13 +11,19 @@ def calcular_salario_devengado(salario_basico, dias_laborados):
     return ((salario_basico/30)*dias_laborados)
     
     
-#Funcion para calcular auxilio de transporte
-def calcular_auxilio_transporte(salario_basico):
+#Funcion para calcular auxilio de transporte 
+"""def calcular_auxilio_transporte(salario_basico):
+    if salario_basico < (2*SALARIO_MINIMO):
+        return 200000 #valor auxilio transporte actual
+    else:
+        return 0"""
+
+#La funcion esta perfecta, pero el nombre mas correcto seria validacion y no calculo, aunque es solo una opinion, no sé que opinen.
+def validar_auxilio_transporte(salario_basico):
     if salario_basico < (2*SALARIO_MINIMO):
         return 200000 #valor auxilio transporte actual
     else:
         return 0
-
 
 #Funcion para calcular descuentos
 def calcular_descuentos(salario_devengado):
@@ -25,8 +31,11 @@ def calcular_descuentos(salario_devengado):
     descuento_pension=salario_devengado*PENSION
     return descuento_salud, descuento_pension
 
-
-
+#Pienso que es mejor hacer los descuentos separados, aunque si lo dejamos así no hay ningún problema.
+"""def descuento_salud(salario_devengado):
+    return salario_devengado*SALUD
+def descuento_pension(salario_devengado):
+    return salario_devengado*PENSION"""
     
     
     
